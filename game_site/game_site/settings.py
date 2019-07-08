30a3ed5d70f1032a4e81785b26b067249b8a8232
Rok Strah <muf@localhost.localdomain>
2019-07-08 19:48:36 +0200

    added basic tic tac toe implementation

--- game_site/game_site/settings.py
+++ game_site/game_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tic_tac_toe.apps.TicTacToeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- game_site/tic_tac_toe/models.py
+++ game_site/tic_tac_toe/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+from typing import Optional
+
+
+class Game(models.Model):
+    # field = models.CharField(default='0'*9, max_length=9)
+    player = 1
+    winner = 0
+    field = [0,0,0,0,0,0,0,0,0]
+
+    def get_position(self, i: int, j: int) -> Optional[int]:
+        if i < 0 or j < 0 or i > 2 or j > 2:
+            return None
+        return 3*i + j
+
+    def toggle_player(self) -> None:
+        self.player = 3 - self.player
+    
+    def check_win(self, i: int, j: int) -> bool:
+        if self.field[j] == self.field[3 + j] == self.field[6 + j] or self.field[3*i] == self.field[3*i + 1] == self.field[3*i + 2]:
+            return True
+        if i == j:
+            if self.field[0] == self.field[4] == self.field[8]:
+                return True
+        if i == 2-j:
+            if self.field[2] == self.field[4] == self.field[6]:
+                return True
+
+    def place(self, p: int) -> bool:
+        if self.field[p] == 0:
+            self.field[p] = self.player
+            return True
+        else:
+            return False
+
+    def move(self, i: int, j: int) -> bool:
+        p = self.get_position(i, j)
+        if p is None or not self.place(p):
+            return False
+        if self.check_win(i, j):
+            self.winner = self.player
+        self.toggle_player()
