3dba8bca84cb7b1756194756daf582bf7c8c1ac2
Rok Strah <muf@localhost.localdomain>
2019-07-09 17:13:44 +0200

    generalised a template and namespaced the app

--- game_site/tic_tac_toe/urls.py
+++ game_site/tic_tac_toe/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'ttt'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('new_game/<str:p1>/<str:p2>/', views.new_game, name='new_game'),
     path('game/<int:game_id>/', views.game, name='game'),
     path('gameover/<int:game_id>/', views.gameover, name='gameover'),
--- game_site/tic_tac_toe/views.py
+++ game_site/tic_tac_toe/views.py
@@ -1,28 +1,35 @@
 from django.shortcuts import render, redirect
+from django.template import loader
+from django.http import HttpResponse
+from django.views import generic
 
 from .models import Game, Player
 
-def index(request):
-    return render(request, 'tic_tac_toe/index.html', {})
+class IndexView(generic.TemplateView):
+    template_name = 'tic_tac_toe/index.html'
 
 def new_game(request, p1, p2):
     g = Game()
     g.p1 = Player.objects.get(name=p1)
     g.p2 = Player.objects.get(name=p2)
     g.save()
-    return redirect(game, g.id)
+    return redirect('ttt:game', g.id)
+
+# class GameView(generic.DetailView):
+#     model = Game
+#     template_name = 'tic_tac_toe/game.html'
 
 def game(request, game_id):
     g = Game.objects.get(id=game_id)
 
     if g.winner != 0 or g.field.count('0') == 0:
-        return redirect(gameover, game_id)
+        return redirect('ttt:gameover', game_id)
 
     conv = {'0': '', '1': 'X', '2': 'O'}
 
     field = [[conv[g.field[3*i + j]] for j in range(3)] for i in range(3)]
-
-    return render(request, 'tic_tac_toe/game.html', {
+    template = loader.get_template('tic_tac_toe/game.html')
+    context = {
         'game_id': game_id,
         'field': field,
         'field0': conv[g.field[0]],
@@ -34,13 +41,29 @@ def game(request, game_id):
         'field6': conv[g.field[6]],
         'field7': conv[g.field[7]],
         'field8': conv[g.field[8]],
-    })
+    }
+    print(template.render(context, request))
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse(g.html())
+    # return render(request, 'tic_tac_toe/game.html', {
+    #     'game_id': game_id,
+    #     'field': field,
+    #     'field0': conv[g.field[0]],
+    #     'field1': conv[g.field[1]],
+    #     'field2': conv[g.field[2]],
+    #     'field3': conv[g.field[3]],
+    #     'field4': conv[g.field[4]],
+    #     'field5': conv[g.field[5]],
+    #     'field6': conv[g.field[6]],
+    #     'field7': conv[g.field[7]],
+    #     'field8': conv[g.field[8]],
+    # })
 
 def play(request, game_id, i, j):
     g = Game.objects.get(id=game_id)
     g.play(i, j)
     g.save()
-    return redirect(game, game_id)
+    return redirect('ttt:game', game_id)
 
 def gameover(request, game_id):
     g = Game.objects.get(id=game_id)
@@ -70,4 +93,4 @@ def create_user(request, name):
     p.name = name
     p.save()
 
-    return redirect(index)
+    return redirect('ttt:index')
