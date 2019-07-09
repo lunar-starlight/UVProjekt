676d7632d79d7af2d69bf0dc59366fc44d758cfb
Rok Strah <muf@localhost.localdomain>
2019-07-09 22:25:23 +0200

    added uttt app to project

--- game_site/game_site/settings.py
+++ game_site/game_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tic_tac_toe.apps.TicTacToeConfig',
+    'ultimate_tic_tac_toe.apps.UltimateTicTacToeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
