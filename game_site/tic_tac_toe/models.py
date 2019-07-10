d2b3e66373e496806d6c3d650fae3c4abc5dd7e1
Rok Strah <muf@localhost.localdomain>
2019-07-10 03:03:19 +0200

    deleted more commented code

--- game_site/tic_tac_toe/models.py
+++ game_site/tic_tac_toe/models.py
@@ -19,10 +19,6 @@ class Game(models.Model):
     keep_score = models.BooleanField(default=True)
     game_over = models.BooleanField(default=False)
 
-    # def __init__(self, p1, p2):
-    #     t1 = Player.objects.get(id=p1)
-    #     t2 = Player.objects.get(id=p2)
-
     def get_position(self, i: int, j: int) -> Optional[int]:
         if i < 0 or j < 0 or i > 2 or j > 2:
             return None
@@ -77,15 +73,3 @@ class Game(models.Model):
         conv = {'0': '', '1': 'X', '2': 'O'}
         context = {'field'+str(i): conv[self.field[i]] for i in range(9)}
         return context
-
-    # def html(self):
-    #     s = '<table>'
-    #     conv = {'0': '', '1': 'X', '2': 'O'}
-    #     field = [[conv[self.field[3*i + j]] for j in range(3)] for i in range(3)]
-    #     for i, row in enumerate(field):
-    #         s += '<tr>'
-    #         for j, el in enumerate(row):
-    #             s += '<td><a style="text-decoration: none; color:black" href="'+ reverse('ttt:play', args=(self.id, i, j)) +'"><div style="border: 1px solid black; width: 60px; height: 60px; font-size: 40px">' + str(el) + '</div></a></td>'
-    #         s += '</tr>'
-    #     s += '</table>'
-    #     return s
--- game_site/tic_tac_toe/urls.py
+++ game_site/tic_tac_toe/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('new_game/<int:p1>/<int:p2>/', views.new_game, name='new_game'),
     path('game/<int:pk>/', views.GameView.as_view(), name='game'),
     path('game/<int:pk>/<int:i>/<int:j>/', views.play, name='play'),
-    # path('user/', views.create_user_form, name='create_user_form'),
     path('user/', views.CreateUserView.as_view(), name='create_user'),
 ]
