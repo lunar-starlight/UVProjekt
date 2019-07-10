5242e02ed10392208d90e449a90d11d485d08775
Rok Strah <muf@localhost.localdomain>
2019-07-10 03:01:32 +0200

    made views more generic

--- game_site/tic_tac_toe/urls.py
+++ game_site/tic_tac_toe/urls.py
@@ -6,9 +6,8 @@ app_name = 'ttt'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('new_game/<int:p1>/<int:p2>/', views.new_game, name='new_game'),
-    path('game/<int:pk>/', views.game, name='game'),
-    path('gameover/<int:pk>/', views.gameover, name='gameover'),
+    path('game/<int:pk>/', views.GameView.as_view(), name='game'),
     path('game/<int:pk>/<int:i>/<int:j>/', views.play, name='play'),
     # path('user/', views.create_user_form, name='create_user_form'),
-    path('user/<str:name>/', views.create_user, name='create_user'),
+    path('user/', views.CreateUserView.as_view(), name='create_user'),
 ]
--- game_site/tic_tac_toe/views.py
+++ game_site/tic_tac_toe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.template import loader
 from django.http import HttpResponse
 from django.views import generic
@@ -8,6 +8,7 @@ from .models import Game, Player
 class IndexView(generic.TemplateView):
     template_name = 'tic_tac_toe/index.html'
 
+
 def new_game(request, p1, p2):
     t1 = get_object_or_404(Player, pk=p1)
     t2 = get_object_or_404(Player, pk=p2)
@@ -15,25 +16,16 @@ def new_game(request, p1, p2):
     g.save()
     return redirect('ttt:game', g.id)
 
-# class GameView(generic.DetailView):
-#     model = Game
-#     template_name = 'tic_tac_toe/game.html'
 
-def game(request, pk):
-    g = get_object_or_404(Game, pk=pk)
+class GameView(generic.DetailView):
+    model = Game
+    template_name = 'tic_tac_toe/game.html'
 
-    if g.winner != 0 or g.field.count('0') == 0:
-        return redirect('ttt:gameover', pk)
 
-    conv = {'0': '', '1': 'X', '2': 'O'}
+# def game(request, pk):
+#     g = get_object_or_404(Game, pk=pk)
+#     return render(request, 'tic_tac_toe/game.html', g.context())
 
-    # field = [[conv[g.field[3*i + j]] for j in range(3)] for i in range(3)]
-    # template = loader.get_template('tic_tac_toe/game.html')
-    # print(template.render(context, request))
-    # return HttpResponse(g.html())
-    context = {'field'+str(i): conv[g.field[i]] for i in range(9)}
-    context['game_id'] = pk
-    return render(request, 'tic_tac_toe/game.html', context)
 
 def play(request, pk, i, j):
     g = Game.objects.get(id=pk)
@@ -41,25 +33,31 @@ def play(request, pk, i, j):
     g.save()
     return redirect('ttt:game', pk)
 
-def gameover(request, pk):
-    g = Game.objects.get(id=pk)
 
-    win = 'The game was tied'
-    if g.winner == 1:
-        win = 'Player 1 won'
-    if g.winner == 2:
-        win = 'Player 2 won'
+# def gameover(request, pk):
+#     g = Game.objects.get(id=pk)
+
+#     win = 'The game was tied'
+#     if g.winner == 1:
+#         win = 'Player 1 won'
+#     if g.winner == 2:
+#         win = 'Player 2 won'
     
-    # Maybe do that elsewhere?
-    # Do I even want this?
-    # g.delete()
+#     # Maybe do that elsewhere?
+#     # Do I even want this?
+#     # g.delete()
 
-    return render(request, 'tic_tac_toe/gameover.html', {'win_str': win})
+#     return render(request, 'tic_tac_toe/gameover.html', {'win_str': win})
 
-def create_user(request, name):
-    p = Player()
+class CreateUserView(generic.CreateView):
+    model = Player
+    fields = ['name']
 
-    p.name = name
-    p.save()
 
-    return redirect('ttt:index')
+# def create_user(request, name):
+#     p = Player()
+
+#     p.name = name
+#     p.save()
+
+#     return redirect('ttt:index')
