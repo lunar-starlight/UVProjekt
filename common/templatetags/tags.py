8c9598da2fcf0c0f692a8169194d30ae3eda3e42
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2019-08-06 20:30:55 +0200

    extracted pagination into seperate template and added `add_get` templatetag
    `add_get` appends arguments to request.GET

--- common/templatetags/tags.py
+++ common/templatetags/tags.py
@@ -16,3 +16,28 @@ def icon(name, style, *args, **kwargs):
 @register.simple_tag
 def is_friend(p1, p2):
     return p1.is_friend(p2)
+
+
+# https://djangosnippets.org/snippets/2428/
+class AddGetParameter(template.Node):
+    def __init__(self, values):
+        self.values = values
+
+    def render(self, context):
+        req = template.Variable('request').resolve(context)
+        params = req.GET.copy()
+        for key, value in self.values.items():
+            resolved = value.resolve(context)
+            if resolved:
+                params[key] = resolved
+        return f'?{params.urlencode()}'
+
+
+@register.tag
+def add_get(parser, token):
+    pairs = token.split_contents()[1:]
+    values = {}
+    for pair in pairs:
+        s = pair.split('=', 1)
+        values[s[0]] = parser.compile_filter(s[1])
+    return AddGetParameter(values)
