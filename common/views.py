lunar-starlight/UVProjekt

commit 3bc5f907ace3ee47283021f37560a50231c0d636

generalised `SearchView` search parameters

common/views.py
@@ -9,6 +9,7 @@ class SearchView(generic.ListView):
     model = get_user_model()
     paginate_by = 10
     queryset = model.objects.all()
+    search_fields = None
 
     def get(self, request, *args, **kwargs):
         if 'search' in request.GET and request.GET['search'] == '':
@@ -25,10 +26,9 @@ class SearchView(generic.ListView):
 
     def get_queryset(self):
         try:
-            if self.request.GET['search'] != '':
+            if self.request.GET['search'] != '' and self.search_fields is not None:
                 self.queryset = self.queryset.annotate(
-                    similarity=TrigramSimilarity('username', self.request.GET['search'])
-                    + TrigramSimilarity('full_name', self.request.GET['search'])
+                    similarity=sum(TrigramSimilarity(s, self.request.GET['search']) for s in self.search_fields)
                 ).filter(similarity__gt=0.3).order_by('-similarity')
         except KeyError:
             pass

connect_four/views.py
@@ -39,6 +39,7 @@ def play(request, pk: int, col: int):
 class NewGameView(LoginRequiredMixin, SearchView):
     template_name = 'connect_four/new_game.html'
     ordering = ['username']
+    search_fields = {'username', 'full_name'}
 
     def get_queryset(self):
         self.queryset = self.request.user.friends.all()

tic_tac_toe/views.py
@@ -50,6 +50,7 @@ def play(request, pk: int, i: int, j: int):
 class NewGameView(LoginRequiredMixin, SearchView):
     template_name = 'tic_tac_toe/new_game.html'
     ordering = ['username']
+    search_fields = {'username', 'full_name'}
 
     def get_queryset(self):
         self.queryset = self.request.user.friends.all()

ultimate_tic_tac_toe/views.py
@@ -77,6 +77,7 @@ def pick(request, pk: int, row: int, col: int, i: int, j: int):
 class NewGameView(LoginRequiredMixin, SearchView):
     template_name = 'ultimate_tic_tac_toe/new_game.html'
     ordering = ['username']
+    search_fields = {'username', 'full_name'}
 
     def get_queryset(self):
         self.queryset = self.request.user.friends.all()

commit 437f6305aefa260dd64f864e90e7e956da21dc8e

fixed leaderboard
broken in 3bc5f907ace3ee47283021f37560a50231c0d636

common/views.py
@@ -28,7 +28,7 @@ class SearchView(generic.ListView):
             if self.request.GET['search'] != '' and self.search_fields is not None:
                 self.queryset = self.queryset.annotate(
                     similarity=sum(TrigramSimilarity(s, self.request.GET['search']) for s in self.search_fields)
-                ).filter(similarity__gt=0.3).order_by('-similarity')
+                ).filter(similarity__gt=0.25).order_by('-similarity')
         except KeyError:
             pass
         return super().get_queryset()
@@ -64,6 +64,8 @@ class BaseIndexView(SearchView):
 class LeaderboardView(SearchView):
     template_name = 'common/leaderboard.html'
     model = get_user_model()
+    queryset = model.objects.all()
+    search_fields = {'username', 'full_name'}
     ordering = ['-wins', 'losses', 'username']
 
 

templates/common/leaderboard.html
@@ -18,22 +18,28 @@
                     <th scope="col">Username</th>
                     <th scope="col">Wins</th>
                     <th scope="col">Losses</th>
-                    <th scope="col">Friends</th>
+                    {% if request.user.is_authenticated %}
+                        <th scope="col">Friends</th>
+                    {% endif %}
                 </tr>
             </thead>
             <tbody>
             {% for player in player_list %}
-                {% is_friend request.user player as is_friend %}
+                {% if request.user.is_authenticated %}
+                    {% is_friend request.user player as is_friend %}
+                {% endif %}
                 <tr>
                     <th scope="row" class="text-right">{{ forloop.counter }}</th>
                     <td>{{ player.get_full_name }}</td>
                     <td>{{ player.username }}</td>
                     <td>{{ player.wins }}</td>
                     <td>{{ player.losses }}</td>
-                    {% if is_friend %}
-                        <td><a href="{% url 'common:remove_friend' player.pk %}"><div>Remove</div></a></td>
-                    {% else %}
-                        <td><a href="{% url 'common:add_friend' player.pk %}"><div>Add</div></a></td>
+                    {% if request.user.is_authenticated %}
+                        {% if is_friend %}
+                            <td><a href="{% url 'common:remove_friend' player.pk %}"><div>Remove</div></a></td>
+                        {% else %}
+                            <td><a href="{% url 'common:add_friend' player.pk %}"><div>Add</div></a></td>
+                        {% endif %}
                     {% endif %}
                 </tr>
             {% endfor %}