49cdea736e539b849de61dd457efb36780226a90
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2019-08-14 00:08:24 +0200

    changed url scheme for TTT ai games

--- tic_tac_toe/urls.py
+++ tic_tac_toe/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('new_game/', NewGameView.as_view(), name='new_game'),
     path('new_game/<int:pk>/', CreateGameView.as_view(), name='new_game'),
-    path('new_game/ai/<slug:slug>/', CreateAIGameView.as_view(), name='new_ai_game'),
+    path('new_game/<slug:slug>/', CreateAIGameView.as_view(), name='new_game'),
     path('game/<int:pk>/', GameView.as_view(), name='game'),
     path('game/<int:pk>/<int:i>/<int:j>/', PlayView.as_view(), name='play'),
 ]
